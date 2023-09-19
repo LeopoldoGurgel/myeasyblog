12ffa27329769092d29d47e56dc8dc7b7e0ed01e
LeopoldoGurgel <leopoldogbp@gmail.com>
2023-09-19 19:56:18 -0400

    created models, controller relations and homeRoutes. Next steps: create user and post routes.

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,4 @@
 -- drop database if exists and create it.
+
+DROP DATABASE IF EXISTS myeasyblog_db;
+CREATE DATABASE myeasyblog_db;
---
+++ models/Comment.js
@@ -0,0 +1,48 @@
+// build comment model to hold it's content, user_id, created_on and post reated to it.
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'post',
+            key: 'id',
+          },
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Comment;
