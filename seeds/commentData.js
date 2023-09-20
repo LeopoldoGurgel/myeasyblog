21884afff19d6a517f43160004c4d31179416753
LeopoldoGurgel <leopoldogbp@gmail.com>
2023-09-20 10:04:10 -0400

    done main.handlebars, server.js and seed data. Next: dashboars and post handlebars

---
+++ seeds/commentData.js
@@ -0,0 +1,17 @@
+[
+    {
+        "content": "That was very insteresting. That's also what I think about that.",
+        "user_id": 2,
+        "post_id": 1
+    },
+    {
+        "content": "I have to read about that",
+        "user_id": 3,
+        "post_id": 1
+    },
+    {
+        "content": "I disagree on that.",
+        "user_id": 4,
+        "post_id": 1
+    }
+]
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,42 @@
-// this file will keep test objects to be sent to the database and populate it and mimic the actual behaviour of the application
+// this file will access test objects saved in files inside this same folder to be sent to the database and populate it and mimic the actual behaviour of the application
+
+const sequelize = require('../config/connection');
+const { User, Comment, Post } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require(`./commentData`);
+
+
+// {force: true} deletes any previous data in the database. For development means only, please.
+
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+
+
+  for (const post of postData) {
+    const posts = await Post.bulkCreate({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+
+    for (const comment of commentData) {
+        await Comment.bulkCreate({
+            ...comment, 
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+            post_id: posts[Math.floor(Math.random() * posts.length)].id,
+        })
+      }
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
